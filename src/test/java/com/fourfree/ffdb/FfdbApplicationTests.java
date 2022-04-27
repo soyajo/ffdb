d36085c8254d9a2bb8e89fd9c20c7c87452ffb09
josoya <thdi4564@nate.com>
2022-04-27 09:03:34 +0900

    Initial commit

---
+++ src/test/java/com/fourfree/ffdb/FfdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourfree.ffdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
